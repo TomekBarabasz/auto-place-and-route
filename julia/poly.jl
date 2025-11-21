c7b52a8a7ec2ca829e712aeda8f744c7c6334c98
Tomek Barabasz <tomek.barabasz@gmail.com>
2025-11-21 13:36:26 +0100

    julia: attempt to make_cnc_drill_path

--- julia/poly.jl
+++ julia/poly.jl
@@ -1,12 +1,12 @@
 module Poly
 
-export make_cnc_path,remove_duplicate_points
+export make_cnc_path,remove_duplicate_points,make_cnc_drill_path
 
 if !isdefined(Main, :Bezier)
     include("Bezier.jl")
 end
 using ..Bezier  # Use parent scope (Main.Bezier)
-
+using DataStructures
 cast(value::Number, ::Type{T}) where T<:Number = T(value)::T
 
 function offset_polyline_simple(points::Vector{Point{T}}, distance) where T <: AbstractFloat
@@ -111,35 +111,105 @@ end
 
 # this requires cw Point order
 function make_cnc_path(points::Vector{Point{T}}, tool_dia, max_error) where T <: AbstractFloat
-    N = [points[i+1]-points[i]|>normalize|>perpendicular for i in 1:length(points)-1]
+    perp(p1::Point{T},p2::Point{T}) where T = p1-p2|>normalize|>perpendicular
     tool_path = Vector{Point{T}}()
     r = cast(tool_dia,T)
     step_a = 2 * acos( (r - max_error) / r )
     thr_cosa = cos(step_a)
-    push!(tool_path, points[begin] + N[begin] * r)
+    Nₚᵣₑᵥ = perp(points[begin+1],points[begin])
+    push!(tool_path, points[begin] + Nₚᵣₑᵥ * r)
     for i in 2:length(points)-1
-        cosa = Bezier.dot(N[i-1],N[i])
-        convex = Bezier.vdot(N[i-1],N[i]) < 0
+        N = perp(points[i+1],points[i])
+        cosa = Bezier.dot(Nₚᵣₑᵥ , N)
+        convex = Bezier.vdot(Nₚᵣₑᵥ , N) < 0
         if convex
-            push!(tool_path, points[i] + N[i-1] * r)
+            push!(tool_path, points[i] + Nₚᵣₑᵥ * r)
             if cosa < thr_cosa
                 a_end = acos(cosa)
                 n_steps = cast(round(a_end / step_a),Int)
                 a = a_end / n_steps
-                n = Bezier.copy(N[i-1])
+                n = Bezier.copy(Nₚᵣₑᵥ)
                 for _ in 1:n_steps-1
                     n = Bezier.rot(n,a)
                     push!(tool_path, points[i] + n * r)
                 end
             end
-            push!(tool_path, points[i] + N[i] * r)
+            push!(tool_path, points[i] + N * r)
         else
             a2 = acos(cosa) / 2
-            n = N[i-1] + N[i] |> normalize
+            n = Nₚᵣₑᵥ + N |> normalize
             push!(tool_path, points[i] + n * Float32(r / cos(a2)))
         end
+        Nₚᵣₑᵥ = N
     end
-    push!(tool_path, points[end] + N[end] * r)
+    push!(tool_path, points[end] + Nₚᵣₑᵥ * r)
+    tool_path
+end
+
+function make_cnc_drill_path(points::Vector{PointF32}, tool_dia, max_error, overlap, n_limit_pts=nothing)
+    perp(p1::Point{T},p2::Point{T}) = p1-p2|>normalize|>perpendicular
+    tool_path = Vector{Point{T}}()
+    r = cast(tool_dia,Float32)
+    step_a = 2*acos( (r - max_error) / r )
+    thr_cosa = cos(step_a)
+    
+    if distance(points[end], points[begin]) > max_error
+        Nₚᵣₑᵥ =  perp(points[end],points[begin])
+        NP = length(points)
+    else
+        Nₚᵣₑᵥ = perp(points[end-1],points[begin])
+        NP = length(points)-1
+    end
+    
+    pts = Deque{Point{T}}()
+    for i in 1:NP
+        push!(pts,points[i])
+    end
+
+    while length(pts) > 1
+        Pt = popfirst!(pts)
+        Ptₙₑₓₜ = first(pts)
+        if distance(Pt,Ptₙₑₓₜ) < max_error
+            break
+        end
+        N = perp(Pt, Ptₙₑₓₜ)
+        cosa = Bezier.dot(Nₚᵣₑᵥ , N)
+        convex = Bezier.vdot(Nₚᵣₑᵥ , N) > 0
+        if convex
+            push!(tool_path, Pt + Nₚᵣₑᵥ * r)
+            if cosa < thr_cosa
+                a_end = acos(cosa)
+                n_steps = cast(round(a_end / step_a),Int)
+                a = - a_end / n_steps
+                n = Bezier.copy(Nₚᵣₑᵥ)
+                for _ in 1:n_steps-1
+                    n = Bezier.rot(n,a)
+                    P1 = Pt + n * r
+                    push!(tool_path, P1)
+                    push!(pts,P1)
+                end
+            end
+            P1 = Pt + N * r
+            push!(tool_path, P1)
+            push!(pts,P1)
+        else
+            a2 = acos(cosa) / 2
+            n = Nₚᵣₑᵥ + N |> normalize
+            P1 = Pt + n * Float32(r / cos(a2))
+            push!(tool_path, P1)
+            push!(pts,P1)
+        end
+        Nₚᵣₑᵥ = N
+        Ptₚᵣₑᵥ = Pt
+
+        if n_limit_pts ≠ nothing && length(tool_path) > n_limit_pts
+            break
+        end
+
+        if length(tool_path) == NP
+            r *= (1.0 - overlap)
+        end
+    end
     tool_path
 end
 
